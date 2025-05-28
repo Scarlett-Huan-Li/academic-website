33f5780f9f26104186b4adfca1e9f46a6deee1b8
Scarlett Li <scarlettlee33@foxmail.com>
2025-05-28 16:58:07 +0200

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dr. Academic Name';
+export const SITE_DESCRIPTION = 'Research in [Your Field] - [University Affiliation]';
+
+export const SOCIAL_LINKS = {
+  googleScholar: 'https://scholar.google.com/citations?user=YOUR_ID',
+  orcid: 'https://orcid.org/YOUR-ORCID-ID',
+  github: 'https://github.com/YOUR_USERNAME',
+  email: 'your.email@university.edu'
+};
